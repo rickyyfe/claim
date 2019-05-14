c1f8e213dcc8dbea0fa52c5d184cd32f7655afb8
rickyyfe <49226151+rickyyfe@users.noreply.github.com>
2019-05-14 15:50:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "firzaananda21o@gmail.com";
+$password = "qwqdff21O";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
